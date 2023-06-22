de6fdabd6f706f44cc0f39f75c50437515d706c0
Viking Studios <68581288+Amateur-God@users.noreply.github.com>
2023-06-22 16:11:43 +0100

    Updated the Mod for UK
    Created faction 2BNB UK
    Added to the 2BNB UK faction
    Medic
    IC
    Rifleman
    
    in camos
    DPM (Woodland)
    DDPM (Desert)
    MTP
    
    Also created groups with 8 man base Section in arrowhead formation for all camo variations

---
+++ addons/UK_TF15/infantry/index.hpp
@@ -0,0 +1,17 @@
+class CfgVehicles {
+	class B_Soldier_F;
+	class B_medic_F;
+	class B_Soldier_SL_F;
+	class UK3CB_BAF_B_Bergen_DPMW_JTAC_H_A;
+	class UK3CB_BAF_B_Bergen_DPMW_Rifleman_A;
+	class UK3CB_BAF_B_Bergen_DPMW_Rifleman_B;
+	class UK3CB_BAF_B_Bergen_MTP_JTAC_H_A;
+	class UK3CB_BAF_B_Bergen_MTP_Medic_L_B;
+	class UK3CB_BAF_B_Bergen_MTP_Rifleman_L_B;
+	class UK3CB_BAF_B_Bergen_DDPM_JTAC_H_A;
+	class UK3CB_BAF_B_Bergen_DDPM_Rifleman_B;
+	class UK3CB_BAF_B_Bergen_DDPM_Rifleman_A;
+	#include "Woodland.hpp" // Woodland Classes
+	#include "Desert.hpp" // Desert classes
+	#include "MTP.hpp" // MTP classes
+};
