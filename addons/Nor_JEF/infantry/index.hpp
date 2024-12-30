b12e6f18910a683d11907d6538295d47db530e40
tmthwoods <84188057+tmthwoods@users.noreply.github.com>
2024-12-30 17:57:08 -0500

    Create index.hpp

---
+++ addons/Nor_JEF/infantry/index.hpp
@@ -0,0 +1,18 @@
+
+class CfgVehicles {
+	class B_Soldier_F;
+	class B_medic_F;
+	class B_Soldier_SL_F;
+	class PB_RT1523BIG_M84;
+	class PB_BP01_M84;
+	class PB_RT1523BIG_Multicam;
+	class PB_BP01_Multicam;
+	class PB_RT1523BIG_M04;
+	class PB_RT1523BIG_M04;
+	class PB_RT1523BIG_SCHNEE;
+	class PB_BP01_Schneetarn;
+	#include "Woodland.hpp" // Woodland Classes
+	#include "Desert.hpp" // Desert classes
+	#include "MTP.hpp" // MTP classes
+	#include "Winter.hpp" // Winter classes
+};
