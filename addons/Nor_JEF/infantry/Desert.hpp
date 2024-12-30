8f2677a933429f54b69cf5f2f8fad74325b3f72a
tmthwoods <84188057+tmthwoods@users.noreply.github.com>
2024-12-30 17:17:23 -0500

    Create Desert.hpp

---
+++ addons/Nor_JEF/infantry/Desert.hpp
@@ -0,0 +1,104 @@
+
+  class BNB_FA_Nor_Rifleman_Desert: B_Soldier_F
+  {
+    faction="BNB_FA_Nor_Faction";
+    editorSubcategory = "BNB_FA_Nor_Desert";
+    side=1;
+    displayName="Rifleman Desert";
+    uniformClass="PB_M04_U";
+    weapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    respawnWeapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    items[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool"};
+    respawnItems[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool"};
+    magazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade"};
+    respawnMagazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade"};
+    linkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","PB_M04_H","PB_M04_Carrier_Rig"};
+    respawnLinkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","PB_M04_H","PB_M04_Carrier_Rig"};
+    backpack="BNB_FA_Nor_Rifleman_Desert_pack";
+  };
+
+  class BNB_FA_Nor_Medic_Desert: B_medic_F
+  {
+    faction="BNB_FA_Nor_Faction";
+    editorSubcategory = "BNB_FA_Nor_Desert";
+    side=1;
+    displayName="Medic Desert";
+    uniformClass="PB_M04_U";
+    weapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    respawnWeapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    items[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool","ACE_surgicalKit"};
+    respawnItems[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool","ACE_surgicalKit"};
+    magazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade"};
+    respawnMagazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade"};
+    linkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","PB_M04_H","PB_M04_Carrier_Rig"};
+    respawnLinkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","PB_M04_H","PB_M04_Carrier_Rig"};
+    backpack="BNB_FA_Nor_Medic_Desert_pack";
+  };
+
+  class BNB_FA_Nor_IC_Desert: B_Soldier_SL_F
+  {
+    faction="BNB_FA_Nor_Faction";
+    editorSubcategory = "BNB_FA_Nor_Desert";
+    side=1;
+    displayName="IC Desert";
+    uniformClass="PB_M04_U";
+    weapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    respawnWeapons[]={"CUP_arifle_HK416_Black","rhsusf_weap_glock17g4","Put","Throw"};
+    items[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool","ACE_HuntIR_monitor","ACE_CableTie","ACE_CableTie"};
+    respawnItems[]={"ACE_adenosine","ACE_EarPlugs","ACE_epinephrine","ACE_epinephrine","ACE_Flashlight_XL50","ACE_MapTools","ACE_morphine","ACE_morphine","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_tourniquet","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_elasticBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_packingBandage","ACE_EntrenchingTool","ACE_HuntIR_monitor","ACE_CableTie","ACE_CableTie"};
+    magazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade","HandGrenade","rhs_mag_M441_HE"};
+    respawnMagazines[]={"CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","CUP_30Rnd_556x45_Stanag","SmokeShell","SmokeShell","SmokeShell","CUP_17Rnd_9x19_glock17","CUP_17Rnd_9x19_glock17","HandGrenade","HandGrenade","HandGrenade","rhs_mag_M441_HE"};
+    linkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","UK3CB_BAF_Soflam_Laserdesignator","PB_M04_H","PB_M04_Carrier_Rig"};
+    respawnLinkedItems[]={"ItemMap","ItemCompass","ItemWatch","TFAR_anprc152","ItemGPS","UK3CB_BAF_Soflam_Laserdesignator","PB_M04_H","PB_M04_Carrier_Rig"};
+    backpack="BNB_FA_Nor_IC_Desert_pack";
+  };
+
+
+  class BNB_FA_Nor_Rifleman_Desert_pack: PB_BP01_M04
+  {
+    scope=2;
+    class TransportMagazines{};
+    class TransportItems{};
+    class TransportWeapons{};
+  };
+
+
+  class BNB_FA_Nor_Medic_Desert_pack: PB_BP01_M04
+  {
+    scope=2;
+    class TransportMagazines{};
+    class TransportItems
+    {
+     class _xx_ACE_plasmaIV_500 {count=6;name="ACE_plasmaIV_500";};
+     class _xx_ACE_plasmaIV_500 {count=2;name="ACE_plasmaIV";};
+     class _xx_ACE_epinephrine {count=6;name="ACE_epinephrine";};
+     class _xx_ACE_adenosine {count=1;name="ACE_adenosine";};
+     class _xx_ACE_morphine {count=6;name="ACE_morphine";};
+     class _xx_ACE_morphine {count=6;name="ACE_painkillers";};
+     class _xx_ACE_elasticBandage {count=25;name="ACE_elasticBandage";};
+     class _xx_ACE_packingBandage {count=15;name="ACE_packingBandage";};
+     class _xx_ACE_tourniquet {count=8;name="ACE_tourniquet";};
+    };
+    class TransportWeapons{};
+  };
+
+
+  class BNB_FA_Nor_IC_Desert_pack: PB_RT1523BIG_M04
+  {
+    scope=2;
+    class TransportMagazines
+    {
+     class _xx_rhs_mag_M433_HEDP {count=6;magazine="rhs_mag_M433_HEDP";};
+     class _xx_rhs_mag_M441_HE {count=6;magazine="rhs_mag_M441_HE";};
+     class _xx_SmokeShell {count=2;magazine="SmokeShell";};
+     class _xx_SmokeShellRed {count=4;magazine="1Rnd_Smoke_Grenade_shell";};
+     class _xx_SmokeShellRed {count=2;magazine="1Rnd_SmokeRed_Grenade_shell";};
+     class _xx_SmokeShellRed {count=4;magazine="ACE_HuntIR_M203";};
+     class _xx_SmokeShellBlue {count=2;magazine="SmokeShellBlue";};
+     class _xx_SmokeShellBlue {count=2;magazine="SmokeShellGreen";};
+
+
+    };
+    class TransportItems{};
+    class TransportWeapons{};
+  };
